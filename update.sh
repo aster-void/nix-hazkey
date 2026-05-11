95140ee4a9da8e4c25032de4aa6a34bb83c62b51
aster-void <137767097+aster-void@users.noreply.github.com>
2026-05-11 17:12:16 +0900

    fix updater so it works now

--- update.sh
+++ update.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# There are multiple packages managed in this distribution.
+# zenzai_v*{,-small}
+# hazkey-*
+# fcitx5-hazkey
+# dictionary
+#
+# out of those, these packages that need to be updated:
+# zenzai-v*: fetches directly from HF = theoretically yes, but these are virtually locked so no
+# fcitx5-hazkey: fetches directly from GH Releases = needs updates
+# hazkey-*, dictionary: derived from fcitx5-hazkey = no
+# Other than that, new zenzai releseases must be added manually to this repository. (not automated)
 echo "Updating prebuilt sources..."
-# corresponds to internal/prebuild/{libllama-cpu,libllama-vulkan,fcitx5-hazkey}.nix
-for pkg in libllama-cpu libllama-vulkan fcitx5-hazkey; do
-  nix-update --flake "$pkg" --commit 
-done
+nix-update --flake fcitx5-hazkey --commit 
