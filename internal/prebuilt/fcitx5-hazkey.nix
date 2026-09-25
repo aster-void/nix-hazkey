ccd76fc0231d9c409b7397987f74f19ce9175b98
aster-void <137767097+aster-void@users.noreply.github.com>
2026-09-25 13:57:04 +0900

    chore: follow upstream hazkey repository rename

--- internal/prebuilt/fcitx5-hazkey.nix
+++ internal/prebuilt/fcitx5-hazkey.nix
@@ -4,7 +4,7 @@ in
   fetchzip {
     name = "fcitx5-hazkey-bin";
     inherit version;
-    url = "https://github.com/7ka-Hiira/fcitx5-hazkey/releases/download/${version}/fcitx5-hazkey-${version}-x86_64.tar.gz";
+    url = "https://github.com/7ka-Hiira/hazkey/releases/download/${version}/fcitx5-hazkey-${version}-x86_64.tar.gz";
     hash = "sha256-jwv1UTRz/FVHmeaumwP45Q4JZcSuZHTrF2/PAzrxeC8=";
     stripRoot = false;
   }
